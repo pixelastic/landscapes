53200ecfb50e3fd558a91d1d6ba44845bdcb273d
Tim Carry <tim@pixelastic.com>
2021-07-26 21:53:10 +0200

    Update metadata

--- src/script.js
+++ src/script.js
@@ -1,4 +1,25 @@
 const theme = require('norska/theme');
+const lazyloadHelper = require('norska/frontend/algolia/helpers/lazyload');
+
 (async () => {
-  await theme.init();
+  await theme.init({
+    hitName: 'picture',
+    placeholder: 'Search for any place, landscape or country',
+    transforms: {
+      img(item) {
+        if (!item.displayPicture) {
+          return false;
+        }
+        const originUrl = item.picture.preview;
+        const options = {
+          imoen: item.displayPicture,
+          uuid: item.objectID,
+          cacheBusting: false,
+        };
+        const img = lazyloadHelper.attributes(originUrl, options);
+
+        return img;
+      },
+    },
+  });
 })();
